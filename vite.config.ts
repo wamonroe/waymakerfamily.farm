aae33d5eca5e6e731412ec25470e290d1e9c74f8
Alex Monroe <alex@monroepost.com>
2026-03-31 12:22:56 -0400

    Switch to Bun
    Mostly for the package manager, but Bun works as a node replacement for everything except building for production.

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter()],
   resolve: {
+    tsconfigPaths: true,
     alias: {
       "@": resolve(__dirname, "src")
     }
